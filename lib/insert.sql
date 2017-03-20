84e1949a5891333da751cc63999c29d25851cce6
Elizabeth Nicholson <elizabethnich19@gmail.com>
2017-03-20 20:23:12 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 40, "M", "dark brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 30, "M", "dark brown", "sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, "F", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 50, "M", "grey", "confused", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 13, "F", "dark brown", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 26, "M", "dark brown", "excited", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 6, "F", "dark brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 20, "M", "black", "aggressive", 0);
